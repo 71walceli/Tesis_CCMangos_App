7dc746548af9c482fc079caf4e316feb55d98a30
Walter Celi <antonini2903@hotmail.com>
2024-03-13 23:04:55 -0500

    Configuración de módulos comines

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,34 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const path = require("path");
 
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {
-  watchFolders: [
-    path.resolve(__dirname),
-    path.resolve(__dirname + "../../../Common"),
-  ],
+const extraNodeModules = {
+  'common': path.resolve(__dirname + '/../Common'),
+};
+const watchFolders = [
+  path.resolve(__dirname),
+  path.resolve(__dirname + '/../Common')
+];
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  }, 
+  resolver: {
+    extraNodeModules,
+    /* extraNodeModules: new Proxy(extraNodeModules, {
+      get: (target, name) =>
+        //redirects dependencies referenced from common/ to local node_modules
+        name in target ? target[name] : path.join(process.cwd(), `node_modules/${name}`),
+    }), */
+    /* modulesPaths: [
+      watchFolders
+    ] */
+  },
+  watchFolders,
 };
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
